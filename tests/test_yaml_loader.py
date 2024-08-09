a18b1736cbb448db201936b4ebabc201d24ac052
Rain <iheyu22@gmail.com>
2024-08-10 07:49:28 +0800

    yaml loader

--- tests/test_yaml_loader.py
+++ tests/test_yaml_loader.py
@@ -5,9 +5,25 @@
 
 import yaml
 
-with open('test.yaml', 'r') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
-    print(data)
-    print(type(data))
-    for key in data:
-        print(key, ':',  data[key])
+
+def test1():
+    with open('test.yaml', 'r') as f:
+        data = yaml.safe_load(f)
+        print(data)
+        print(type(data))
+        for key in data:
+            print(key, ':', data[key])
+
+
+def test2():
+    with open('test2.yaml', 'r', encoding='utf-8') as f:
+        data = yaml.safe_load(f)
+        print(data)
+        print(type(data))
+        for stage in data:
+            print(stage, ':', data[stage])
+
+
+if __name__ == '__main__':
+    test1()
+    test2()
